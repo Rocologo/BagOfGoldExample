04283e2aa6eaaaa732daa09b26231b2abb08e0b3
Rocologo <rocologo@rocologo.tk>
2022-08-15 09:06:01 +0200

    Compiled

--- src/one/lindegaard/BagOfGoldExample/BackOfGoldExample.java
+++ src/one/lindegaard/BagOfGoldExample/BackOfGoldExample.java
@@ -10,18 +10,17 @@ import one.lindegaard.BagOfGold.api.BagOfGoldAPI;
 
 public class BackOfGoldExample extends JavaPlugin {
 
-	private static BackOfGoldExample plugin;
 	private static BagOfGoldAPI bagOfGoldAPI;
 
 	@Override
 	public void onEnable() {
-		Plugin bagOfGold = Bukkit.getServer().getPluginManager().getPlugin("BagOfGold");
-		if ((bagOfGold != null) && (bagOfGold.isEnabled())) {
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("BagOfGold");
+		if ((plugin != null) && (plugin.isEnabled())) {
 			Bukkit.getConsoleSender()
 					.sendMessage(ChatColor.GOLD + "[BagOfGoldExample] " + ChatColor.RESET + "Hooked into BagOfGold.");
 
 			bagOfGoldAPI = BagOfGold.getAPI();
-			
+				
 			// Now do something with bagOfGoldAPI
 			// 
 			// The API interface is here:  https://github.com/Rocologo/BagOfGold/tree/master/src/one/lindegaard/BagOfGold/api/BagOfGoldAPI.java
